c8ba981dcbc3c8d30ed3e1b64c638e338a2bf504
Michael Chalupa <mchalupa3@gmail.com>
2019-04-12 08:28:30 -0500

    udpate

---
+++ src/MathPractice.java
@@ -0,0 +1,18 @@
+public class MathPractice {
+    public static void main(String[] args){
+        int num1, num2, num3;
+
+        num1 = 10;
+
+        num2 = 35;
+
+        num3 = num1 + num2;
+
+        System.out.println( num3 );
+
+        num3 = num1 + num2;
+
+        System.out.println( num3 );
+
+    }
+}
